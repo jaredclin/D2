6324d615570bde02ed864adc7f36620a8af2935e
jaredclin <jaredc0307@gmail.com>
2019-02-26 00:26:23 -0500

    Delete test.rb

--- test.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'location'
-xxx = Location.new(3)
-puts xxx.get_fake_rubies(0)
-puts xxx.get_fake_rubies(1)
-puts xxx.get_fake_rubies(2)
-puts xxx.get_fake_rubies(3)
-puts xxx.get_fake_rubies(4)
-puts xxx.get_fake_rubies(5)
-puts xxx.get_fake_rubies(6)
