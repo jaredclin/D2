7e97b1125a118eecbcff2a21636821527ae0ac98
jaredclin <jaredc0307@gmail.com>
2019-03-02 14:20:59 -0500

    Update command_line_test.rb
    minor comment fix

--- command_line_test.rb
+++ command_line_test.rb
@@ -5,6 +5,7 @@ require_relative 'command_line'
 class CommandLineTest < Minitest::Test
 
   # UNIT TEST FOR METHOD report_correct_usage
+  # If report_correct_usage is called, output will match the correct usage message specified per the requirements
   def test_report_correct_usage
     assert_output("Usage:\nruby gold_rush.rb *seed* *num_prospectors* *num_turns*\n*seed* should be an integer\n*num_prospectors* should be a non-negative integer\n*num_turns* should be a non-negative integer\n") { CommandLine.report_correct_usage }
   end
